e390d4a059f5c885db5e77e71404cb9715e1b961
Kirita Escott <Kirita.Escott@ecs.vuw.ac.nz>
2017-09-27 13:45:53 +1300

    basic consumer drawing on the GUI with semi finalised interface

--- Library/src/nz/ac/victoria/swen424/gui/gui.java
+++ Library/src/nz/ac/victoria/swen424/gui/gui.java
@@ -5,6 +5,7 @@ import java.awt.event.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -23,45 +24,24 @@ public class gui extends JPanel {
    ArrayList <ElConsumer> _consumers = new ArrayList <ElConsumer>();
    
    //Image files paths for each MainType
-   private final String conImage = "/consumer.png";
+   private final String conImage = "/images/house.png";
    private final String tranImage = "/images/";
    private final String prodImage = "/images/";
    private final String gridImage = "/images/";
    
    public void paint(Graphics g) {
-//      for (int f = 0; f < type.length; f++) { 
-//         for (int s = 0; s < styles.length; s++) { 
-//            Font font = new Font(type[f], styles[s], 18);
-//            g.setFont(font); 
-//            String name = type[f] + " " + stylenames[s];
-//            g.drawString(name, 20, (f * 4 + s + 1) * 20); 
-//         }
-//      }
       //stuff to paint a consumer on the pane
-	   File f = new File("consumer2.png");
-	   Path currentRelativePath = Paths.get("");
-	   String s = currentRelativePath.toAbsolutePath().toString();
-	   System.out.println("Current relative path is: " + s);
-	 
-      BufferedImage img = null;
-      try {
-    	  System.out.println("trying to read image");
-    	  System.out.println("wheres the image: "+s+conImage);
-    	  	img = ImageIO.read(f);
-    	  	System.out.println("reads image "+img.toString());
-      } catch(IOException e) {
-    	  	System.out.println(e);
-      }
-	   
-	
-      
-//      int w = img.getWidth();
-//      int h = img.getHeight();
-//      BufferedImage bi = new
-//          BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
-//      g = bi.getGraphics();
-//      g.drawImage(img, 0, 0, null);
-      
+	   BufferedImage img =null;
+	   try {
+		img = ImageIO.read(new File(getClass().getResource(conImage).toURI()));
+	} catch (IOException e1) {
+		// TODO Auto-generated catch block
+		e1.printStackTrace();
+	} catch (URISyntaxException e1) {
+		// TODO Auto-generated catch block
+		e1.printStackTrace();
+	}
+      g.drawImage(img, 0, 0, 50, 50, null);    
    }
    public static void main(String[] args) {
       JFrame f = new JFrame();
