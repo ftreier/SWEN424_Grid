4cc0eabfef6f0c6873e23e29a3c132ccbb0030b4
Kirita Escott <Kirita.Escott@ecs.vuw.ac.nz>
2017-10-07 05:09:48 +1300

    removed redundant classes/methods in relation to rendering GUI, will work on actual rendering later today, code still builds fine etc. note: to run my simulation you will need to change to file source path where the output.xml is saved/read from

--- Library/src/nz/ac/victoria/swen424/gui/Window.java
+++ Library/src/nz/ac/victoria/swen424/gui/Window.java
@@ -17,54 +17,11 @@ import javax.swing.JPanel;
 import nz.ac.victoria.swen424.mainTypes.ElProducer;
 
 /**
- * Contains all objects
+ * Window to render objects on. 
  * */
-public class Window extends JPanel {
-	public static Component com;
-	private List <ElProducer> producer;
-	private final String prodImage = "/images/powerplant.png";
-	
-	public void displaySimulation() {
-	       JFrame f= new JFrame("PowerGrid Simulation");    
-	        JPanel panel=new JPanel();  
-	        panel.setBounds(0,700,1250,50);    
-	        panel.setBackground(Color.gray);  
-	        JButton b1=new JButton("Start");
-	        //set this to be a nicer layout
-	        b1.setBounds(10,100,80,30);    
-	        b1.setBackground(Color.yellow);
-	        Component com = this.com;
-	        panel.add(com);
-	        panel.add(b1);  
-	        f.add(panel);  
-	                f.setSize(1250,750);    
-	                f.setLayout(null);    
-	                f.setVisible(true); 
-	}
-	
-  public void paint(Graphics g) {
-  //stuff to paint a consumer on the pane
-   BufferedImage img =null;
-   int x=0; 
-   int y=50;
-   
-   for (ElProducer p : producer) {
-	   try {
-			img = ImageIO.read(new File(getClass().getResource(prodImage).toURI()));
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		} catch (URISyntaxException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		   //drawimage(img, x, y, width, height, null)
-	   	  x = x+100;
-	      g.drawImage(img, x, y, 50, 50, null); 
-	      //g.drawString(e.getData(), x+5, y+60);
-   }
-   
-}
+public class Window {
+	private JFrame mainFrame;
+	private JFrame footerFrame;
 	
 
 }
